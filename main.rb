2c4f1c31b700134aa0033597404a28c3324a934b
Bri In The Sky <brian@orville.lan>
2015-04-13 15:43:34 +0100

    Refactored dbcode for neatness

--- main.rb
+++ main.rb
@@ -18,6 +18,15 @@ require './datalayerlight.rb'
 
 class Anagrams
 
+  attr_accessor :hostname, :username, :password, :dbname
+  
+  def initialize
+    @hostname = "127.0.0.1"
+    @username = "monetdb"
+    @password = "monetdb"
+    @dbname = "anagrams"
+  end
+  
   def perms(word, maxgenperms)
     unless @quiet == false; print "\e[1;34mWord:\e[0m\ \e[1;32m#{word}\e[0m\ \e[1;34mPermutations:\e[0m\ "; end
     #print "Word: #{word}"
@@ -52,14 +61,31 @@ class Anagrams
     unless @quiet == false; print "\n"; end
   end
 
+  def dbconnect
+    @conn = DatalayerLight.new
+    @conn.hostname = @hostname
+    @conn.username = @username
+    @conn.password = @password
+    @conn.dbname = @dbname
+    @conn.autocommit = false
+    begin
+      @conn.connect
+    rescue Errno::ECONNREFUSED
+      puts "Database not running!"
+      puts "Bye!"
+      exit
+    end
+  end
+
+  def dbclose
+    if @conn.autocommit == false; @conn.commit; end
+    @conn.save
+    @conn.release
+    @conn.close
+  end
+  
   def readfile(name)
-    conn = DatalayerLight.new
-    conn.hostname = "127.0.0.1"
-    conn.username = "monetdb"
-    conn.password = "monetdb"
-    conn.dbname = "anagrams"
-    conn.autocommit = true
-    conn.connect 
+    self.dbconnect
     File.open("#{name}", 'r') {|t|
       t.each_line {|l|
         w = l.strip
@@ -67,7 +93,7 @@ class Anagrams
           c = self.perms("#{w}", 150)
           self.show(c)
           begin
-            conn.query("#{@sql}")
+            @conn.query("#{@sql}")
             unless @quiet == false; print "\e[1;30mSQL: #{@sql[0..160]}\e[0m\ \n"; end
           rescue
             puts "DB Error"
@@ -76,10 +102,7 @@ class Anagrams
         end
       }
     }
-    if conn.autocommit == false; conn.commit; end
-    conn.save
-    conn.release
-    conn.close
+    self.dbclose
   end
 
 end
