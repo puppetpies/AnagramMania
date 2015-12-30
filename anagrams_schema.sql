e93b04412c2766e66518d7602f5f466b2ad2a42b
Bri in The Sky <brianh6854@googlemail.com>
2015-12-30 22:20:37 +0000

    Added wordjumble table

--- anagrams_schema.sql
+++ anagrams_schema.sql
@@ -12,4 +12,15 @@ word_id INT GENERATED ALWAYS AS
   anagrams string not null
 );
 
-
+DROP TABLE "anagrams".wordjumble;
+CREATE TABLE "anagrams".wordjumble (
+jid INT GENERATED ALWAYS AS 
+        IDENTITY (
+           START WITH 0 INCREMENT BY 1
+           NO MINVALUE NO MAXVALUE
+           CACHE 2 CYCLE
+) primary key,
+  word_id int not null,
+  word varchar(200) not null,
+  FOREIGN KEY (word_id) REFERENCES "anagrams".words (word_id)
+);
